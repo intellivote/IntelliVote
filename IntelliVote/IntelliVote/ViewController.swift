0f851ce7a47230473f63b93dc150009df9e583df
Yaniv Bronshtein <yanbronshtein@gmail.com>
2019-04-08 20:12:22 -0400

    Created Organization and Initial README correctly

---
+++ IntelliVote/IntelliVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntelliVote
+//
+//  Created by Dean Pektas on 4/5/19.
+//  Copyright © 2019 Dean Pektas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
