1ba785e555535df091ef7c2a1531275ad38aa155
dean.pektas27 <dean.pektas27@qmail.cuny.edu>
2019-04-19 17:04:21 -0400

    Added Google API and grabbed voting location text

--- IntelliVote/IntelliVote/ViewController.swift
+++ IntelliVote/IntelliVote/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var voteAddressText: UITextField!
+    
+    @IBOutlet weak var pollLocationBox: UITextView!
+    
+    var vote = [[String:Any]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print ("Hello")
+        
+        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/voterinfo?address=20108+23rd+Ave.+Bayside+NY&electionId=2000&officialOnly=true&returnAllAvailableData=true&fields=contests%2CdropOffLocations%2CearlyVoteSites%2Celection%2Ckind%2CmailOnly%2CnormalizedInput%2CotherElections%2CpollingLocations%2CprecinctId%2Csegments%2Cstate&key=AIzaSyB9-Z4TVPVAvaqu8p0_Q-iyMBcDLuNTZxo")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                self.vote = dataDictionary["pollingLocations"] as! [[String: Any]]
+                let voteinfo = self.vote[0]
+                let addr = voteinfo["address"] as! [String: Any]
+                let locName = addr["locationName"] as? String
+                self.pollLocationBox.text = locName
+                print(locName)
+                //let nestedDictionary = self.vote["address"] as? [String:Any]
+                
+                
+                
+                
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
+        
     }
-
-
 }
+
+
 
