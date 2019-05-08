b9bf7ff0c88c7d81f9b986697f125de72c1309e0
dean.pektas27 <dean.pektas27@qmail.cuny.edu>
2019-05-08 16:27:40 -0400

    Generified URL

--- IntelliVote/IntelliVote/VotingLocationViewController.swift
+++ IntelliVote/IntelliVote/VotingLocationViewController.swift
@@ -17,10 +17,22 @@ class VotingLocationViewController: UIViewController {
     var vote = [[String:Any]]()
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let part1URL = "https://www.googleapis.com/civicinfo/v2/voterinfo?address="
         
-        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/voterinfo?address=20108+23rd+Ave.+Bayside+NY&electionId=2000&officialOnly=true&returnAllAvailableData=true&fields=contests%2CdropOffLocations%2CearlyVoteSites%2Celection%2Ckind%2CmailOnly%2CnormalizedInput%2CotherElections%2CpollingLocations%2CprecinctId%2Csegments%2Cstate&key=AIzaSyB9-Z4TVPVAvaqu8p0_Q-iyMBcDLuNTZxo")!
+        let part2URL = "&electionId=2000&officialOnly=true&returnAllAvailableData=true&fields=contests%2CdropOffLocations%2CearlyVoteSites%2Celection%2Ckind%2CmailOnly%2CnormalizedInput%2CotherElections%2CpollingLocations%2CprecinctId%2Csegments%2Cstate&key=AIzaSyB9-Z4TVPVAvaqu8p0_Q-iyMBcDLuNTZxo"
+        
+        let myAddress = "2411 21st Ave. Astoria NY"
+        
+        let modifiedAddress = myAddress.replacingOccurrences(of: " ", with: "+", options: .literal
+            , range: nil)
+        let stringURL = part1URL + modifiedAddress + part2URL
+        
+        
+        
+        let url = URL(string:stringURL )!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
