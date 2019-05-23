781e3ce146952e3f5a12ead1dc5a1050ce1ba127
Mohanad <mohanad.osman08@qmail.cuny.edu>
2019-05-23 19:05:39 -0400

    Worked a bit on the API call for the candidate List controller

--- IntelliVote/IntelliVote/CandidateListViewController.swift
+++ IntelliVote/IntelliVote/CandidateListViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
-
-class CandidateListViewController: UIViewController {
+import AlamofireImage
+import Parse
+class CandidateListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let APIKey = "AIzaSyDaBUfYL90FvXCO9bfQ-7qJYON-d2yiDwo"
+    
+    var contests = [[String : Any]]()
+    
+    var candidates = [[String: Any]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        loadCandidates()
+        
+        self.collectionView.reloadData()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func loadCandidates() {
+        let part1CivicsURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?address="
+        let currentUser = PFUser.current()
+        let currentUserAddress = currentUser?["address"] as! String
+        
+        let modifiedUserAddress = currentUserAddress.replacingOccurrences(of: " ", with: "+", options: .literal
+            , range: nil)
+        let part2CivicsURL = "+NY&electionId=2000&officialOnly=true&returnAllAvailableData=true&fields=contests%2CdropOffLocations%2CearlyVoteSites%2Celection%2Ckind%2CmailOnly%2CnormalizedInput%2CotherElections%2CpollingLocations%2CprecinctId%2Csegments%2Cstate&key="
+        
+        
+        let stringCivicsURL = part1CivicsURL + modifiedUserAddress + part2CivicsURL + self.APIKey
+        
+        let url = URL(string:stringCivicsURL )!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.contests = dataDictionary["contests"] as! [[String: Any]]
+                self.candidates = [self.contests[3] ]
+
+                
+                
+//                let candidateInfo = self.contests["candidates"] as? [[String:Any]]
+//                let candidateName = candidateInfo[4] as! [String: Any]
+//                print(candidateName)
+                
+            }
+        }
+            task.resume()
+            
     }
     
 
@@ -27,5 +81,26 @@ class CandidateListViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return contests.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "candidateViewCell", for: indexPath) as! CandidateCollectionViewCell
+        let candidate = candidates[indexPath.row]
+        print(candidate)
+//        let candidateName = self.candidates["name"] as! String
+//        let candidateParty = self.candidates["party"] as! String
+//        let candidatePhotoURL = self.candidates["photoUrl"] as! String
+        
+        
+        
+
+        return cell
+    }
 
 }
