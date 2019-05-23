7f7efc418fa7b00694187d573bd3c67dddba6af9
Mohanad <mohanad.osman08@qmail.cuny.edu>
2019-05-23 16:16:15 -0400

    Touched up on the Candidate List View Controller and set up the referencing variables

---
+++ IntelliVote/IntelliVote/CandidateListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CandidateListViewController.swift
+//  IntelliVote
+//
+//  Created by Mohanad Osman on 5/23/19.
+//  Copyright © 2019 Dean Pektas. All rights reserved.
+//
+
+import UIKit
+
+class CandidateListViewController: UIViewController {
+    
+    @IBOutlet weak var candidatePhoto: UIImageView!
+    
+    @IBOutlet weak var candidateName: UILabel!
+    
+    @IBOutlet weak var candidateParty: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
